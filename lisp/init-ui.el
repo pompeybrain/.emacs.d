8e2d97a383862fc88c9afc15183df30dd630102a
pompeybrain <pompeybrain@gmail.com>
2018-12-26 15:58:18 +0800

    improve ui

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -7,7 +7,6 @@
       '((:eval (if (buffer-file-name)
                    (abbreviate-file-name (buffer-file-name))
                  "%b"))))
-;; (setq frame-title-format '("%b"))
 
 ;; 关闭工具栏，tool-bar-mode 即为一个 Minor Mode
 (tool-bar-mode -1)
@@ -18,17 +17,20 @@
 ;; 关闭文件滑动控件
 (scroll-bar-mode -1)
 
-
 ;; 显示行号
 (global-linum-mode 1)
 
 ;; set font
-(set-face-attribute 'default nil :family "Source Code Pro")
+(set-face-attribute 'default nil :family "Consolas")
 (set-face-attribute 'default nil :height 150)
+(set-face-attribute 'default nil :weight 'normal)
+(setq-default line-spacing 4)
 
+;; set cursor
+(setq-default cursor-type '(bar . 2))
 
-;; 关闭启动帮助画面
-;; (setq inhibit-splash-screen 1)
+;; set pair match face
+(set-face-background 'show-paren-match "#55575e")
 
 (provide 'init-ui)
 ;;; init-ui.el ends here
