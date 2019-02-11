ee98040ab1dc54eeb1f058033e6af4645ade87c8
pompeybrain <pompeybrain@gmail.com>
2019-02-11 10:39:16 +0800

    add target

--- custom.el
+++ custom.el
@@ -6,24 +6,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#21252B" "#E06C75" "#98C379" "#E5C07B" "#61AFEF" "#C678DD" "#56B6C2" "#ABB2BF"])
  '(custom-safe-themes
-   '("a4f9fa4e912ae4d8620255928b786a3caaa9d4ddb722693bcafff003383b9ee7" "10f345cd3bd7556debbf3e6c0e5292963ffb889d634f40ec95999601cae8cfd4" "4532af80e41942590a514cf0a3c5ef50a8085ad116401c3781e1a097afeb9d68" "10c660db5a2f74330581c2c3a75ae4f2f8c2133dd97e0d9c3c47c42b8183b24a" "a036e61d7e93757283404fb314f59952e06301b130d8ef5526dabf4631e61ed5" "8335d045e0b067d6545a153ae3f8385c7b564d09c5f44e07e5442bcaf7072a7d" "33a51aba3452e95a876d63f4a909f18e367f8536697ebb0bc18a68b2e0ae0ae5" "9396b22672a8897a1d39815d6949941136627a0a2b4148b738592acbb192f712" "cebef4dec2cf99ae497d857c43860d66ea6a9b71d7e46a177577294e974bc2a1" "6811488f27513befa8b8c43595ad2e846dc5f50e8b050ca918ad6ba6bcfa2864" "028de01489a683696c64dcc2a01eaa663670d04202de3fce48ec3a5542bc2da5" "2f1e1eb6d6c32a8a3ed1d8f0f97be9c15d0836ced9245a84ade5068d6340254e" "100dbaf9135237019d86d5409cac7485a7b8d05daf218dcaa2ed98fc46ba8523" "5e2b58dac894a6ce435b64fc5c279bcc24d542a4df12cd7686d8cfaba17b80da" "e036631f7eca7d8635cec52ee0d203c036bcdf6a05f40bfe8458eaec9b2f3f4b" "f8cf128fa0ef7e61b5546d12bb8ea1584c80ac313db38867b6e774d1d38c73db" "0e0667a47b8145e68edba656fff8d170dcb965cf39a8a07f807a8598ee230300" "316d92ebce0ce4ef1a12f811b73611c4947079b24c5719a76acf5f45e41aaf23" "fadc74c4d787f864ab78ac68b39ea56c6256b438151cf6a71e858b570f18a542" "73d7d6b515411509716b76c6a638b69f56a47ad9935d0e041886150a644e8369" "88748605d1675281f69da19e09d637f81eba6ab7a1ccb22eb1ab59387c7898e9" "f34654c859e5f6d69d5b077aa4c58b4a03988eb92d4f0140ddc3120515e183af" "5acd11a664b04de6fe0ccf3016b869cfad8978e60b99e9939df6640760043743" "a645ded4fb932d16e5089f09a6c8d754f0af9f8d9afbb6429c60fcd83525710f" "f35a7f66d4afe791793138e72e3ad1517a4ae91dd9224cd7b0273fe705b73acd" "64f04b5337724aa74ee88ff01498817209928d37df478ca0fcc9bd87582343ab" "14cacd83c0c20d21124fa02e0aa3a8d78e1ac65668b5429a5398cb63df0db3ac" "9e34173b81de6aefd27d7e7820bbc0fa03b8f8f02506159d79beee350ae95158" "05d751033cf7fe2a3a966f785963af2bce80af2022abd19d58e1b88c4827c8dd" "5df31a691f2488b764724b374d004f2603d1088130620f1e44d24820cd1bdee0" "117582a6f106233d89d66d3a5039fb443f9f80fd0f0200d033d9a62e40c748b2" "92e769ae1fc5816298a6da4dc30fa0d818e57988a93f064913af472225792b16" "358ed1744f1a16a5b8acb039f6b76f9c928e9aecbd360efa0f70291a74ce30e4" "198a7b1562cfc3f058bb289187e2fe7483f594d74d1bbac551931de4a0e5f21b" "bf5bdab33a008333648512df0d2b9d9710bdfba12f6a768c7d2c438e1092b633" default))
- '(fci-rule-color "#3E4451")
- '(hl-sexp-background-color "#efebe9")
- '(package-selected-packages
-   '(leuven-theme markdown-mode vue-html-mode helpful posframe popup yaml-mode dart-mode multi-web-mode vue-mode counsel-projectile projectile anzu smex diff-hl emmet-mode atom-one-dark-theme hl-todo web-mode git-gutter editorconfig diminish treemacs which-key nlinum-hl nlinum org swiper ivy flycheck-pos-tip esup benchmark-init counsel rainbow-mode prettier-js tide js2-mode magit typescript-mode exec-path-from-shell eglot lsp-mode flycheck fly-check geiser delight auto-package-update use-package company-lisp lsp-ui company-lsp yasnippet rainbow-delimiters paredit neotree helm company all-the-icons ace-jump-mode))
- '(tetris-x-colors
-   [[229 192 123]
-    [97 175 239]
-    [209 154 102]
-    [224 108 117]
-    [152 195 121]
-    [198 120 221]
-    [86 182 194]]))
+   '("a4f9fa4e912ae4d8620255928b786a3caaa9d4ddb722693bcafff003383b9ee7" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
