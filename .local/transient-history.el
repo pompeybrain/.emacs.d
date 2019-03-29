864f9d80f5cb3e901ae880ad433689456145bd4a
pompeybrain <pompeybrain@gmail.com>
2019-03-30 00:19:23 +0800

    remove local dir from repo

--- .local/transient-history.el
+++
@@ -1,8 +0,0 @@
-((magit-commit nil)
- (magit-dispatch nil)
- (magit-pull nil)
- (magit-push nil)
- (magit-reset nil)
- (magit-stash nil
-              ("--include-untracked"))
- (magit-submodule nil))
